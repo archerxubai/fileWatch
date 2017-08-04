608a551bb36527cca61b7f86077c2d36ac79d65f
hasee <bailongxubai@gmail.com>
2017-08-04 21:26:34 +0800

    first commit

---
+++ express_server/app.js
@@ -0,0 +1,108 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const bodyParser = require('body-parser')
+const session = require('cookie-session')
+
+const fs = require('fs')
+
+const {log} = require('../utils')
+const { secretKey } = require('./config')
+
+// 先初始化一个 express 实例
+const app = express()
+
+// 设置 bodyParser
+app.use(bodyParser.urlencoded({
+    extended: true,
+}))
+
+// 配置 nunjucks 模板, 第一个参数是模板文件的路径
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app,
+})
+
+// messageList 用来存储所有的 message
+const messageList = []
+
+const picture = require('./routes/picture')
+
+app.use('/picture', picture)
+
+// 定义路由和路由处理函数的方式如下
+// 用 app.METHODS 的方式定义请求
+// METHODS 只接受 get/post/delete/update 方法（当然我们使用 get 和 post 就可以了）
+// 第一个参数是 path, 第二个参数是一个回调函数, 这个回调函数是用来处理请求的
+// 比如这个就是处理 path 为 / 的 get 请求
+app.get('/', (request, response) => {
+    // response.send 发送的是 response 的内容
+    // 相当于我们用的 socket.write 方法
+    log('route /')
+    response.send('Hello Gua')
+})
+
+// 这是访问 /message 的请求
+// method 是 get
+app.get('/message', (request, response) => {
+    // 打印请求的方法 GET 或者 POST
+    log('请求方法', request.method)
+
+    // request.query 是 express 保存 URL 中的参数的属性
+    // 访问 http://127.0.0.1:2000/message?msg=hello
+    // 会打印出对象格式的数据
+    /*
+    {
+        msg: 'hello'
+    }
+    */
+    log('request, query 参数', request.query)
+    // response.render 会渲染一个模板文件, 因为前面已经设置了模板文件的路径
+    // 所以实际上会去找 templates/message_index.html 文件
+    // 第二个参数是一个对象, 会把模板里的 messages 变量替换成 messageList 的值
+    response.render('message_index.html', {
+        messages: messageList,
+    })
+})
+
+// 这个路由函数只支持 post 方法, path 为 /message/add
+app.post('/message/add', (request, response) => {
+    // 打印请求的方法 GET 或者 POST
+    log('message_add 请求方法', request.method)
+
+    // request.body 是 express 中保存 POST 请求的表单数据的属性
+    // 默认是 undefined
+    // 需要安装 bodyParser 或者其他中间件才能获取正确的数据
+    log('request, POST 的 form 表单数据', request.body)
+    // 把数据生成一个 dict 存到 message_list 中去
+    const msg = {
+        content: request.body.msg_post || '',
+    }
+    messageList.push(msg)
+    // 使用 response.redirect 函数来重定向, 这个和我们写过的函数是一样的
+    response.redirect('/message')
+})
+
+// 把逻辑放在单独的函数中, 这样可以方便地调用
+// 指定了默认的 host 和 port, 因为用的是默认参数, 当然可以在调用的时候传其他的值
+const run = (port=3000, host='') => {
+    // app.listen 方法返回一个 http.Server 对象, 这样使用更方便
+    // 实际上这个东西的底层是我们以前写的 net.Server 对象
+    const server = app.listen(port, host, () => {
+        // 非常熟悉的方法
+        const address = server.address()
+        host = address.address
+        port = address.port
+        log(`listening server at http://${host}:${port}`)
+    })
+}
+
+if (require.main === module) {
+    const port = 4000
+    // host 参数指定为 '0.0.0.0' 可以让别的机器访问你的代码
+    const host = '0.0.0.0'
+    run(port, host)
+}
+
+module.exports = {
+    run: run,
+}
---
+++ express_server/dirWatch.js
@@ -0,0 +1,107 @@
+const watch = require('node-watch')
+const fs = require('fs')
+
+const { log } = require('../utils')
+
+const ensureExists = (path) => {
+    if (!fs.existsSync(path)) {
+        const data = '[]'
+        fs.writeFileSync(path, data)
+    }
+}
+
+const save = (data, path) => {
+    const s = JSON.stringify(data, null, 2)
+    fs.writeFileSync(path, s)
+}
+
+const load = (path) => {
+    const options = {
+        encoding: 'utf8',
+    }
+    ensureExists(path)
+    const s = fs.readFileSync(path, options)
+    const data = JSON.parse(s)
+    return data
+}
+
+let pictures = []
+
+// 返回文件夹中的文件名
+const filesIn = (path) => {
+    let files = fs.readdirSync(path)
+    return files
+}
+
+
+
+let newFiles = []
+let filesUpload = load('./db/filesUpload.txt')
+
+// 监视文件夹，如果有新的文件加入，若不在已filesUpload中，则加入newFiles列表。
+const dirWatch = (path) => {
+    watch(path, (event, name) => {
+        if (event == 'update') {
+            let files = filesIn(path)
+            files.map((f) => {
+                if (!filesUpload.includes(f)) {
+                    newFiles.push(f)
+                }
+            })
+        }
+        log('newFiles', newFiles)
+        log('filesUpload', filesUpload)
+    })
+}
+
+const upload = (filePath, serverPath ) => {
+    const net = require('net')
+
+// 设置连接服务器的信息
+    const host = '115.182.201.8'
+    const port = 80
+
+// 创建一个客户端, 可以连接到服务器
+    const client = new net.Socket()
+
+// 客户端根据给出的配置参数打开一个连接, 这样可以连接到服务器
+    client.connect(port, host, () => {
+        console.log('connect to: ', host, port)
+
+        // 向服务器发送一个消息
+        const request = 'data from client'
+        client.write(request)
+
+        // 如果 server destroy 之后, 再调用下面的代码会报错
+        // setInterval(() => {
+        //     client.write('hello in interval')
+        // }, 100)
+    })
+
+// 当接收服务器的响应数据时触发 data 事件
+    client.on('data', (d) => {
+        // 参数是 d, 默认情况下是 buffer 类型
+        // 可以用 d.toString() 将 buffer 转成字符串
+        console.log('data:', d, d.toString())
+
+        // 完全关闭 client 的连接, 套路写法
+        client.destroy()
+    })
+
+// client 关闭的时候触发这个事件
+    client.on('close', function () {
+        console.log('connection closes')
+    })
+}
+
+const test = () =>{
+    log(newFiles)
+}
+
+const _main = () =>{
+
+}
+
+module.exports = {
+    filesIn: filesIn
+}
